90347bec8088cc799d0dfc56517f7a95006aab28
Sharpenergit <udemycourse1999@gmail.com>
2023-03-27 09:31:18 +0530

    pb5

---
+++ pb5.js
@@ -0,0 +1,5 @@
+let element=document.getElementsByClassName('abc');
+element[0].textContent='item5byclassname';
+
+let element1=document.getElementsByTagName('li');
+element1[4].textContent='item5bytagname';
